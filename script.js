eccb4c992a69b83187927e5b0f35e6731dc9aa94
Sofia Medina <medinasofiaa1@gmail.com>
2023-05-25 11:52:50 -0300

    commit actualizado

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-const USUARIO_NOMBRE_REGISTRADO = "carla";
-const USUARIO_CONTRASENIA_REGISTRADO= "Carla1.";
+/* const USUARIO_NOMBRE_REGISTRADO = "carla";
+const USUARIO_CONTRASENIA_REGISTRADO= ;
 const USUARIO_DOS_NOMBRE_REGISTRADO = "juan";
-const USUARIO_DOS_CONTRASENIA_REGISTRADA = "juan1." ;
+const USUARIO_DOS_CONTRASENIA_REGISTRADA =  ; */
+
+/* let usuarioNombreRegistrado= prompt("Ingresar un usuario");
+let usuarioContraseniaRegistrado = prompt ("Ingresar una constraseña "); */
+
+const nomUsuario = document.querySelector("#contenedor-usuario");
+const inputContrasenia = document.querySelector("#contrasenia");
+
+const usuario = ["carla", "juan"];
+const constrasenias = ["Carla1.", "juan1."];
+
+function verificar() {
+
+    let usuarioIngresado = nomUsuario.value;
+    let contraseniaIngresada = inputContrasenia.value;
+
+    for (let i = 0; i < usuario.length; i++) {
+         
+        if ((usuarioIngresado == (usuario[i])) && (contraseniaIngresada == constrasenias[i])) {
+            document.write("contraseña y usuarios correctos");
+        } else {
+            document.write("constraseña o constraseña incorrectos");
+        }
+    }
+}
 
 
-let usuarioNombreRegistrado= prompt("Ingresar un usuario");
-let usuarioContraseniaRegistrado = prompt ("Ingresar una constraseña ");
-
-
-
-if (((usuarioContraseniaRegistrado == USUARIO_CONTRASENIA_REGISTRADO) && (USUARIO_NOMBRE_REGISTRADO == usuarioNombreRegistrado)) || ((usuarioNombreRegistrado == USUARIO_DOS_NOMBRE_REGISTRADO) && (usuarioContraseniaRegistrado == USUARIO_DOS_CONTRASENIA_REGISTRADA)) ) {
+/* if (((usuarioContraseniaRegistrado == USUARIO_CONTRASENIA_REGISTRADO) && (USUARIO_NOMBRE_REGISTRADO == usuarioNombreRegistrado)) || ((usuarioNombreRegistrado == USUARIO_DOS_NOMBRE_REGISTRADO) && (usuarioContraseniaRegistrado == USUARIO_DOS_CONTRASENIA_REGISTRADA)) ) {
     document.write ("contraseña y usuarios correctos");
 } else {
     document.write ("constraseña o constraseña incorrectos");
-}
- 
+} */
+
+
